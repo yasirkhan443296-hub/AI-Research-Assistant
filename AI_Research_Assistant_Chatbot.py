55f87bf405b86a94d39e73b2a84841a350770a1a
Farhan Khan <yasirkhan443296@gmail.com>
2026-08-01 08:19:31 +0530

    Update AI_Research_Assistant_Chatbot.py

--- AI_Research_Assistant_Chatbot.py
+++ AI_Research_Assistant_Chatbot.py
@@ -90,7 +90,7 @@ def load_llm():
     llm = ChatGroq(
         model="llama-3.3-70b-versatile",
         temperature=0.5,
-        api_key=os.getenv("GROQ_API_KEY"),
+        api_key=api_key
     )
     return llm
 
